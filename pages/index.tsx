10c52960db46b38f2463f98d2bb6c139b959c81c
drnachio <nacho@binpar.com>
2021-09-16 17:33:05 +0200

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,19 @@ const index: React.FC<IndexProps> = ({title}) => (
       <link rel="stylesheet" type="text/css" href="/styles.css" />
     </Head>
     <h1>{title}</h1>
+    <div className="block blockA"></div>
+    <div className="block blockB"></div>
+    <div className="block blockC"></div>
+    <div className="block blockD"></div>
+    <br style={{clear:'both'}} />
+    <div className="block blockA dark"></div>
+    <div className="block blockB dark"></div>
+    <div className="block blockC dark"></div>
+    <div className="block blockD dark"></div>
+    <br style={{clear:'both'}} />
+    <div className="mainBlock">
+      <p>Voluptate sit aute id aute exercitation aliqua pariatur commodo aliqua incididunt tempor. Id cillum esse mollit id proident. Ipsum officia officia anim irure et ea aute pariatur eiusmod mollit aliqua. Magna qui sint deserunt id deserunt ullamco magna quis laborum ex sunt sint. Quis ea aliquip consequat do excepteur. Elit magna qui magna nostrud incididunt laboris commodo id ea ullamco reprehenderit quis id.</p>
+    </div>
   </React.Fragment>
 );
 
